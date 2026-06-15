b69ea22c9b33b3784241b62890f8d3cad8e0cdd7
Henrique-zoo <morcelleshenrique@gmail.com>
2026-06-15 18:22:31 -0300

    feat(auth): adiciona uma camada "policy" para autorização

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,33 @@
+class ApplicationPolicy
+  attr_reader :usuario, :record
+
+  def initialize(usuario, record)
+    @usuario = usuario
+    @record = record
+  end
+
+  def self.scope(usuario, scope)
+    const_get(:Scope).new(usuario, scope).resolve
+  end
+
+  def adm?
+    usuario&.administrador?
+  end
+
+  def current_adm
+    usuario&.perfil_adm
+  end
+
+  class Scope
+    attr_reader :usuario, :scope
+
+    def initialize(usuario, scope)
+      @usuario = usuario
+      @scope = scope
+    end
+
+    def resolve
+      scope.none
+    end
+  end
+end
