798a53270029f330481d5de50be822c966ab0398
PedroFFranca <pedrofreifra@gmail.com>
2026-06-15 22:52:55 -0300

    feat (controller): adicionando o controler do escolher publico alvo

--- app/controllers/formularios_controller.rb
+++ app/controllers/formularios_controller.rb
@@ -6,16 +6,57 @@ class FormulariosController < ApplicationController
     @turmas = Turma.do_semestre_atual.sem_formulario.includes(:materia)
   end
 
-  def create
-    Formularios::CreateFromTemplate.call(
+  def preparar
+    Formularios::CreateFromTemplate.validate_preparacao!(
       template_id: params[:template_id],
-      turma_ids: params[:turma_ids],
+      turma_ids: params[:turma_ids]
+    )
+
+    session[:formulario_preparacao] = {
+      "template_id" => params[:template_id].to_i,
+      "turma_ids" => Array(params[:turma_ids]).map(&:to_i)
+    }
+
+    redirect_to publicar_formularios_path
+  rescue Formularios::Error => e
+    redirect_to new_formulario_path, alert: e.message
+  end
+
+  def publicar
+    preparacao = session[:formulario_preparacao]
+    unless preparacao
+      redirect_to new_formulario_path
+      return
+    end
+
+    @template = Template.find(preparacao["template_id"])
+    @turmas = Turma.where(id: preparacao["turma_ids"]).includes(:materia)
+  end
+
+  def create
+    preparacao = session[:formulario_preparacao]
+    unless preparacao
+      redirect_to new_formulario_path, alert: "Selecione um template e as turmas antes de publicar"
+      return
+    end
+
+    Formularios::CreateFromTemplate.call(
+      template_id: preparacao["template_id"],
+      turma_ids: preparacao["turma_ids"],
+      publico_alvo: params[:publico_alvo],
       perfil_adm: current_usuario.perfil_adm
     )
+
+    session.delete(:formulario_preparacao)
 
     redirect_to new_formulario_path,
                 notice: "Formulário criado com sucesso para as turmas selecionadas"
   rescue Formularios::Error => e
-    redirect_to new_formulario_path, alert: e.message
+    if e.message == Formularios::CreateFromTemplate::SEM_PUBLICO_ALVO
+      redirect_to publicar_formularios_path, alert: e.message
+    else
+      session.delete(:formulario_preparacao)
+      redirect_to new_formulario_path, alert: e.message
+    end
   end
 end
